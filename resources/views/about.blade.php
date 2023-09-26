cc3ab854df27ca375383ade571499a242a710f75
unknown <putrigsi30a@gmail.com>
2023-09-26 12:28:58 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>putrii</title>
+</head>
+<body>
+    <h2> <?= $name; ?> </h2>
+    <p> <?= $email; ?> </p>
+    <img src="img/<?= $img; ?> " alt="<?= $name; ?>" width="30%">
+</body>
+</html>
